127fdbdb3842ba2e26f3226144beb305c86aaf6b
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-04-15 22:36:04 -0300

    Create lib/blog.ex file

---
+++ src/lib/blog.ex
@@ -0,0 +1,14 @@
+defmodule Dhony.Blog do
+  alias Dhony.Post
+
+  use NimblePublisher,
+    build: Post,
+    from: "./posts/**/*.md",
+    as: :posts,
+    highlighters: [:makeup_elixir, :makeup_erlang]
+
+  @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
+
+  # And finally export them
+  def all_posts, do: @posts
+end
