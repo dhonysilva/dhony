5c0a79e4c1fcddc3bcd344dd58642a227460e429
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-04-15 23:02:34 -0300

    Adding post, index and layout functions to dhony.ex

--- src/lib/dhony.ex
+++ src/lib/dhony.ex
@@ -6,4 +6,38 @@ defmodule Dhony do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  use Phonenix.Component
+  import Phonenix.HTML
+
+  def post(assigns) do
+    ~H"""
+      <.layout>
+        <%= raw @post.body %>
+      </.layout>
+    """
+  end
+
+  def index(assigns) do
+    ~H"""
+    <.layout>
+      <h1>Jason's Personal website!!</h1>
+      <h2>Posts!</h2>
+      <ul>
+        <li :for={post <- @posts}>
+          <a href={post.path}> <%= post.title %> </a>
+        </li>
+      </ul>
+    </.layout>
+    """
+  end
+
+  def layout(assigns) do
+    ~H"""
+    <html>
+      <body>
+        <%= render_slot(@inner_block) %>
+      </body>
+    </html>
+    """
+  end
 end
